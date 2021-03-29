21cc2b7ef881e028842d698af4098fe68eddadea
belyakovAP <belyak0vap@yandex.ru>
2021-03-29 12:47:12 +0500

    age calc

--- test_pyprj/venv/test_file.py
+++ test_pyprj/venv/test_file.py
@@ -1,21 +1,11 @@
-friends_names = ['Аня', 'Коля', 'Лёша', 'Лена', 'Миша']
-friends_cities = ['Владивосток', 'Красноярск', 'Москва', 'Обнинск', 'Чебоксары']
+import datetime as dt
 
-friends = {}
+born_time = dt.datetime(1987, 12, 13)
+current_time = dt.datetime.utcnow()
 
-# допишите ваш код сюда
-#for i in friends_names:
-#    for j in friends_cities:
-#        string = f'{friends_names[i]} : {friends_cities[j]}'
-#    friends.append(string)
-#print(friends)
+print(born_time)
+print(current_time)
 
-#for names, cities in zip(friends_names, friends_cities):
-#    friends[names] = cities
-#print(friends)
-#print(f"Лена живёт в городе {friends['Лена']}")
+age = current_time - born_time
 
-for i in range(0, len(friends_names)):
-    friends[friends_names[i]] = friends_cities[i]
-#print(friends)
-print(f"Лена живёт в городе {friends['Лена']}")
+print(f'Мой возраст {age}')
